98354ce4b264de38154ce7eb4760104952e1de49
Daniel Feliz <47199869+dfeliz@users.noreply.github.com>
2021-12-27 02:04:08 +0000

    add metamask connection

--- src/App.js
+++ src/App.js
@@ -6,23 +6,59 @@ import './App.css';
 export default function App() {
   const [donations, _setDonations] = useState("0");
   const [quantity, _setQuantity] = useState("");
+  const [currentAccount, setCurrentAccount] = useState("");
 
   const addDonation = () => {
     const sum = Number(donations) + Number(quantity);
     _setDonations(sum.toFixed(8).toString());
   }
 
-  const checkIfWalletIsConnected = () => {
-    /*
-    * First make sure we have access to window.ethereum
-    */
-    const { ethereum } = window;
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window;
 
-    if (!ethereum) {
-      console.log("Make sure you have metamask!");
-      return;
-    } else {
-      console.log("We have the ethereum object", ethereum);
+      if (!ethereum) {
+        console.log("Make sure you have metamask!");
+        return;
+      } else {
+        console.log("We have the ethereum object", ethereum);
+      }
+
+      /*
+      * Check if we're authorized to access the user's wallet
+      */
+      const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Found an authorized account:", account);
+        setCurrentAccount(account)
+      } else {
+        console.log("No authorized account found")
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  /**
+  * Implement your connectWallet method here
+  */
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error)
     }
   }
 
@@ -41,7 +77,7 @@ export default function App() {
 
       <div className="dataContainer">
         <div className="header">
-          Donations app!
+          Ethereum donations
         </div>
 
         <div className="bio">
@@ -49,28 +85,45 @@ export default function App() {
         </div>
 
         <div className="bio">
-          Total donations until now: {donations}
+          <p>
+            Total donations until now: {donations}
+          </p>
+          <div className="currency">
+            <img
+              className="icon"
+              src={ethereumIcon}
+            />
+            <p>ETH</p>
+          </div>
         </div>
 
         <div className="actionsContainer">
-          <input
-            type="number"
-            className="input"
-            value={quantity}
-            step="0.00000001"
-            placeholder="0.00000001"
-            onChange={(e) => setQuantity(e.target.value)}
-            onInput={(e) => setQuantity(e.target.value)}
-          >
-          </input>
-          <img
-            className="icon"
-            src={ethereumIcon}
-          />
-          <p>ETH</p>
-          <button className="waveButton" onClick={addDonation}>
-            Donate
+          {!currentAccount ? (
+            <button className="waveButton" onClick={connectWallet}>
+              Connect Wallet
           </button>
+          ) : (
+            <>
+              <input
+                type="number"
+                className="input"
+                value={quantity}
+                step="0.00000001"
+                placeholder="0.00000001"
+                onChange={(e) => setQuantity(e.target.value)}
+                onInput={(e) => setQuantity(e.target.value)}
+              >
+              </input>
+              <img
+                className="icon"
+                src={ethereumIcon}
+              />
+              <p>ETH</p>
+              <button className="waveButton" onClick={addDonation}>
+                Donate
+            </button>
+          </>
+        )}
         </div>
       </div>
     </div>
